363ba4dcaf00604f1fe475ad6eb8e97c21c183e2
Praise Aribisala <aribisalapraise12@gmail.com>
2025-04-19 16:30:06 +0100

    - Implement balance count up - Implement doughnut chart

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,25 @@
-import React from 'react'
+import HeaderBox from "@/components/HeaderBox";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
 
 const HomePage = () => {
+    const loggedIn = { firstName: "Praise" }
   return (
-    <section>
-        <div>
-            <div>Home Page</div>
+    <section className="home">
+        <div className='home-content'>
+            <div className='home-header'>
+                <HeaderBox 
+                    type="greeting"
+                    title="Welcome"
+                    user={loggedIn?.firstName || 'Guest'}
+                    subtext="Access and manage your account and transactions efficiently."
+                />
+
+                <TotalBalanceBox 
+                    accounts={[]}
+                    totalBanks={1}
+                    totalCurrentBalance={1250.35}
+                />
+            </div>
         </div>
     </section>
   )
