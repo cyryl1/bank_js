21ec6129c8d97f22244f8a3d7a37721a7af5112c
Praise Aribisala <aribisalapraise12@gmail.com>
2025-04-19 12:39:02 +0100

    Initial File structure

---
+++ app/(root)/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const HomePage = () => {
+  return (
+    <section>
+        <div>
+            <div>Home Page</div>
+        </div>
+    </section>
+  )
+}
+
+export default HomePage
